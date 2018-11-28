86bc1bfb7a5e15cbbaf6163efe0fe961deb0f363
John <john.alamina@hud.ac.uk>
2018-11-28 01:56:45 +0000

    Books web page

---
+++ dblaze.Shared/DBContext/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dblaze.Shared.DBContext
+{
+    public partial class Book
+    {
+        public string BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int? CategoryId { get; set; }
+        public decimal? Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
